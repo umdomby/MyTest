23fe890b71729a789745aad5cab9d06ee52d3dcd
Navatar <umdomby@yandex.ru>
2025-04-09 13:27:16 +0300

    edit

--- app/src/main/java/com/example/mytest/MainActivity.kt
+++ app/src/main/java/com/example/mytest/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.mytest
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.media.projection.MediaProjectionManager
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
@@ -15,6 +16,7 @@ class MainActivity : ComponentActivity() {
         Manifest.permission.CAMERA,
         Manifest.permission.RECORD_AUDIO,
         Manifest.permission.POST_NOTIFICATIONS,
+        Manifest.permission.FOREGROUND_SERVICE_MEDIA_PROJECTION,
         Manifest.permission.FOREGROUND_SERVICE_MICROPHONE,
         Manifest.permission.FOREGROUND_SERVICE_CAMERA
     )
@@ -30,16 +32,29 @@ class MainActivity : ComponentActivity() {
             finish()
         }
     }
-
+    private val mediaProjectionLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            startWebRTCService()
+        } else {
+            showToast("Media projection permission denied")
+            finish()
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         if (checkAllPermissionsGranted()) {
-            startWebRTCService()
-            showToast("Service started successfully")
+            requestMediaProjection()
         } else {
             requestPermissionLauncher.launch(requiredPermissions)
         }
+    }
+
+    private fun requestMediaProjection() {
+        val mediaManager = getSystemService(MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
+        mediaProjectionLauncher.launch(mediaManager.createScreenCaptureIntent())
     }
 
     private fun startWebRTCService() {
